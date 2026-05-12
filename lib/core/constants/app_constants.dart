c6bddd70746e1d8c72fa7f4f3422173a6551f7aa
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-05-13 00:14:51 +0530

    Add AppConstants and AppColors classes for application constants and color definitions

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  const AppConstants._();
+
+  static const appName = 'Blooom';
+  static const tagline = 'Menstrual Health Companion';
+  static const logoAsset = 'assets/app-logo.png';
+}
+
+class AppColors {
+  const AppColors._();
+
+  static const rose50 = Color(0xFFFFF1F4);
+  static const rose100 = Color(0xFFFFE3E9);
+  static const rose200 = Color(0xFFFFCCD9);
+  static const rose300 = Color(0xFFFFA6BD);
+  static const rose400 = Color(0xFFFF6E96);
+  static const rose500 = Color(0xFFFF3B74);
+  static const lavender = Color(0xFF7684FF);
+  static const sky = Color(0xFF8BC6FF);
+  static const mint = Color(0xFF83E889);
+  static const lemon = Color(0xFFFFE56D);
+  static const ink = Color(0xFF141B34);
+}
