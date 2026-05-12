28af9fe479184b73501133d924c50e2f7e887e9a
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-05-13 00:14:46 +0530

    Add BlooomApp class to initialize the application with AppViewModel

---
+++ lib/app/blooom_app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../core/theme/app_theme.dart';
+import '../presentation/pages/home_shell_page.dart';
+import '../presentation/viewmodels/app_scope.dart';
+import '../presentation/viewmodels/app_view_model.dart';
+
+class BlooomApp extends StatelessWidget {
+  const BlooomApp({super.key, required this.viewModel});
+
+  final AppViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScope(
+      viewModel: viewModel,
+      child: AnimatedBuilder(
+        animation: viewModel,
+        builder: (context, _) {
+          return MaterialApp(
+            title: 'Blooom',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.light,
+            darkTheme: AppTheme.dark,
+            themeMode: viewModel.themeMode,
+            home: const HomeShellPage(),
+          );
+        },
+      ),
+    );
+  }
+}
